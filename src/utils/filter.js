2f880ebeae12b1fd54e441ecbaf31d21c1f5a41c
Daniil Matveev <ihatemat@yandex.ru>
2020-09-30 22:42:29 +0430

    commit 4 gh-pages

--- src/utils/filter.js
+++
@@ -1,8 +0,0 @@
-import {FilterType} from "../const";
-
-export const filter = {
-  [FilterType.ALL]: (films) => films,
-  [FilterType.WATCHLIST]: (films) => films.filter((film) => film.isInWatchlist),
-  [FilterType.HISTORY]: (films) => films.filter((film) => film.isWatched),
-  [FilterType.FAVORITES]: (films) => films.filter((film)=> film.isFavorite),
-};
